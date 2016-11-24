ab41e4c7b9cf4d53de8400771b48bd35f82d00c9
tykowale <tykowale@gmail.com>
2016-11-24 12:40:52 -0600

    update error on creation for user

--- test/controllers/user_controller_test.exs
+++ test/controllers/user_controller_test.exs
@@ -7,14 +7,24 @@ defmodule Todos.UserControllerTest do
 
     test "#create renders when resource is valid" do
         conn = build_conn()
-        user = @valid_attrs
 
-        conn = post(conn, user_path(conn, :create, user), %{user: user})
+        conn = post(conn, user_path(conn, :create, @valid_attrs), %{user: @valid_attrs})
         response_body = response_body_to_map(conn.resp_body, "user")
 
         assert json_response(conn, 200) == render_json(UserView, "show.json", user: response_body)
 
         users = Repo.all(User)
         assert(length(users) == 1)
+    end
+
+    test "#create when there is an error creating" do
+        conn = build_conn()
+        user = %{@valid_attrs | email: ""}
+
+        conn = post(conn, user_path(conn, :create, user), %{user: user})
+        assert response(conn, 401) == "There was an error creating your user"
+
+        users = Repo.all(User)
+        assert(length(users) == 0)
     end
 end
--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -3,15 +3,15 @@ defmodule Todos.UserController do
     alias Todos.User
 
     def create(conn, %{"user" => user_params}) do
-        changeset = User.changeset(%User{}, user_params)
+        changeset = User.registration_changeset(%User{}, user_params)
 
         case Repo.insert(changeset) do
             {:ok, user} ->
                 conn
                 |> render("show.json", user: user)
-            {:error, changeset} ->
+            {:error, _} ->
                 conn
-                |> send_resp(400, [])
+                |> send_resp(401, "There was an error creating your user")
         end
     end
 end
